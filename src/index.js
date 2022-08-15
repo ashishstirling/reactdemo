906f5bb3d02f006a6b57065159f96e61848e206a
ashishstirling <ashishkulkarni@hotmail.com>
2022-08-15 17:42:17 +0100

    Add files via upload
    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const restEndpoint = "http://demoapplication-env.eba-cwwcvivr.us-east-1.elasticbeanstalk.com/hello";
+
+const callRestApi = async () => {
+    const response = await fetch(restEndpoint);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    return JSON.stringify(jsonResponse);
+};
+
+function RenderResult() {
+  const [apiResponse, setApiResponse] = useState("Loading MicroService Response...");
+
+  useEffect(() => {
+      callRestApi().then(
+          result => setApiResponse(result));
+  },[]);
+
+  return(
+      <div>
+          <h1>React App</h1>
+          <p>{apiResponse}</p>
+      </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RenderResult />
+  </React.StrictMode>
+);
